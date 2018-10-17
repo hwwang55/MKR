e0437ade2cd152338cb53ecf5633b66ae998f0e7
hwwang55 <wanghongwei55@gmail.com>
2018-10-17 19:35:40 +0800

    Add Last.FM dataset

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ parser.add_argument('--dataset', type=str, default='movie', help='which dataset 
 parser.add_argument('--n_epochs', type=int, default=20, help='the number of epochs')
 parser.add_argument('--dim', type=int, default=8, help='dimension of user and entity embeddings')
 parser.add_argument('--L', type=int, default=1, help='number of low layers')
-parser.add_argument('--H', type=int, default=2, help='number of high layers')
+parser.add_argument('--H', type=int, default=1, help='number of high layers')
 parser.add_argument('--batch_size', type=int, default=4096, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-6, help='weight of l2 regularization')
 parser.add_argument('--lr_rs', type=float, default=0.02, help='learning rate of RS task')
@@ -26,7 +26,7 @@ parser.add_argument('--dataset', type=str, default='book', help='which dataset t
 parser.add_argument('--n_epochs', type=int, default=5, help='the number of epochs')
 parser.add_argument('--dim', type=int, default=8, help='dimension of user and entity embeddings')
 parser.add_argument('--L', type=int, default=1, help='number of low layers')
-parser.add_argument('--H', type=int, default=2, help='number of high layers')
+parser.add_argument('--H', type=int, default=1, help='number of high layers')
 parser.add_argument('--batch_size', type=int, default=32, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-6, help='weight of l2 regularization')
 parser.add_argument('--lr_rs', type=float, default=2e-4, help='learning rate of RS task')
@@ -34,6 +34,19 @@ parser.add_argument('--lr_kge', type=float, default=2e-5, help='learning rate of
 parser.add_argument('--kge_interval', type=int, default=2, help='training interval of KGE task')
 '''
 
+'''
+# music
+parser.add_argument('--dataset', type=str, default='music', help='which dataset to use')
+parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
+parser.add_argument('--dim', type=int, default=4, help='dimension of user and entity embeddings')
+parser.add_argument('--L', type=int, default=2, help='number of low layers')
+parser.add_argument('--H', type=int, default=1, help='number of high layers')
+parser.add_argument('--batch_size', type=int, default=256, help='batch size')
+parser.add_argument('--l2_weight', type=float, default=1e-6, help='weight of l2 regularization')
+parser.add_argument('--lr_rs', type=float, default=1e-3, help='learning rate of RS task')
+parser.add_argument('--lr_kge', type=float, default=2e-4, help='learning rate of KGE task')
+parser.add_argument('--kge_interval', type=int, default=2, help='training interval of KGE task')
+'''
 
 show_loss = False
 show_topk = False
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,9 +1,12 @@
 import argparse
 import numpy as np
 
-RATING_FILE_NAME = dict({'movie': 'ratings.dat', 'book': 'BX-Book-Ratings.csv', 'news': 'ratings.txt'})
-SEP = dict({'movie': '::', 'book': ';', 'news': '\t'})
-THRESHOLD = dict({'movie': 4, 'book': 0, 'news': 0})
+RATING_FILE_NAME = dict({'movie': 'ratings.dat',
+                         'book': 'BX-Book-Ratings.csv',
+                         'music': 'user_artists.dat',
+                         'news': 'ratings.txt'})
+SEP = dict({'movie': '::', 'book': ';', 'music': '\t', 'news': '\t'})
+THRESHOLD = dict({'movie': 4, 'book': 0, 'music': 0, 'news': 0})
 
 
 def read_item_index_to_entity_id_file():
@@ -111,7 +114,7 @@ if __name__ == '__main__':
     np.random.seed(555)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', type=str, default='book', help='which dataset to preprocess')
+    parser.add_argument('-d', type=str, default='movie', help='which dataset to preprocess')
     args = parser.parse_args()
     DATASET = args.d
 
